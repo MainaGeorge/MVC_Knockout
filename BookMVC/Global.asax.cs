b89408972d0833fae47925bf446ba75ae3d3c5e7
George <nuriszka@gmail.com>
2022-06-05 15:58:55 +0200

    Add project files.

---
+++ BookMVC/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using BookMVC.DAL;
+using System.Data.Entity;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace BookMVC
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var bookContext = new BookContext();
+            Database.SetInitializer(new BookInitializer());
+
+            bookContext.Database.Initialize(true);
+        }
+    }
+}
